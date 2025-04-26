03b833e490045496e195b38996803d6082d29689
Priyanka <priyankabhasakhetre80@gmail.com>
2025-04-26 11:16:30 +0530

    Created NewMOnkey app and fetching latest news using newsApi key

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <News key="general" country="us" pageSize={5} category="general" />
+          </Route>
+          <Route exact path="/general">
+            <News key="general" country="us" pageSize={5} category="general" />
+          </Route>
+          <Route exact path="/business">
+            <News
+              key="business"
+              country="us"
+              pageSize={5}
+              category="business"
+            />
+          </Route>
+          <Route exact path="/entertainment">
+            <News
+              key="entertainment"
+              country="us"
+              pageSize={5}
+              category="entertainment"
+            />
+          </Route>
+          <Route exact path="/science">
+            <News key="science" country="us" pageSize={5} category="science" />
+          </Route>
+          <Route exact path="/sports">
+            <News key="sports" country="us" pageSize={5} category="sports" />
+          </Route>
+          <Route exact path="/health">
+            <News key="health" country="us" pageSize={5} category="health" />
+          </Route>
+          <Route exact path="/technology">
+            <News
+              key="technology"
+              country="us"
+              pageSize={5}
+              category="technology"
+            />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/NewsItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, urlToImage, url, author, date, source } = this.props;
+    return (
+      <div>
+        <div className="card">
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '95%', zIndex: '1'}}>
+    {source.name}
+  </span>
+          <img
+            src={
+              urlToImage
+                ? urlToImage
+                : "https://sportsfly.cbsistatic.com/fly-0945/bundles/sportsmediacss/images/fantasy/default-article-image-large.png"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body" style={{ maxHeight: "10%" }}>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text"><small className="text-muted">By {author? author : 'unknown'} on {new Date(date).toGMTString()}</small></p>
+            <a href={url} rel="noreferrer" target="_blank" className="btn btn-primary btn-dark">
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
